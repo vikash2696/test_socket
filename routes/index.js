23ebe413227dc0f2764be65a32cdb0fcd8a6ef78
vikash2696 <vikash2696@gmail.com>
2017-12-29 12:56:17 +0530

    done

--- routes/index.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var Router = express.Router();
-var questionRoutes = require('./questionRoutes');
-
-Router.use('/questions', questionRoutes);
-
-module.exports = Router;
